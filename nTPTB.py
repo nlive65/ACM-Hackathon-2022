3f8075c135286fa4c58190580ebded95d611c796
nlive65 <94426110+nlive65@users.noreply.github.com>
2022-03-26 19:45:50 -0500

    Add files via upload

--- nTPTB.py
+++ nTPTB.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
 import json
 import os
-
 from lib2to3.pgen2.tokenize import tokenize
 import translators as translate
+
+
 
 
 if os.path.exists(os.getcwd() + "/config.json"):
@@ -14,55 +15,65 @@ else:
     with open(os.getcwd() + "/config.json","w+") as f:
         json.dump(configTemplate,f)
 
-bot = commands.Bot(command_prefix='$', help_command=None)
+
+bot = commands.Bot(command_prefix='$', help_command = None)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
+async def help(ctx, target_language = 'en'):
+    
+    return await ctx.send("yay")
 
 class translator(commands.Cog):
     def __init__(self,bot): 
+        global langDict
         self.bot = bot
+        file = open('countries.txt','r')
+        langDict = {}
+        f = file.readlines()
+        for line in f:
+            words = line.split()
+            langDict[words[0].lower()] = words[1]
+        file.close()
+    @commands.command(name = "help")
+    async def help(self, ctx, target_language = 'en'):
+        return await ctx.send("yay")
+
 
     @commands.command(name='hello')
     async def test(self,ctx):
+        """ returns hello"""
         return await ctx.send("Hello, world!")
 
     @commands.command(name='echo')
     async def echo(self, ctx, *, message):
         return await ctx.send(message)
    
-    @commands.command(name='translate', aliases = ['tl', 'tr'])
+    @commands.command(name='translate', aliases = ['tl', 'Tl', 'tL', 'TL'])
     async def translate(self, ctx, from_language, to_language, *, message):
         translated_message = translate.google(message, str(from_language), str(to_language))
-        return await ctx.send(translated_message)
+        return await ctx.send(translated_message[0])
 
-    @commands.command(name = 'TranslateTTs', aliases = ['tltts', 'translatetts', 'translateTTS', 'translateTTs'])
-    async def translateTTS(self,ctx, from_language, to_language, *, message):
+    @commands.command(name = 'TranslateTTs', aliases = ['tltts'])
+    async def translateTTS(self,ctx, from_language, to_language,*, message):
          translated_message = translate.google(message, str(from_language), str(to_language))
          return await ctx.send(translated_message, tts=True)
 
-    @commands.command(name = 'languageGuide', aliases = ['lg', 'languageguide','languages'])
+
+    @commands.command(name = 'languagGuide', aliases = ['lG', 'lg', 'Lg'])
     async def countryGuide(self, ctx):
-        file = open("languages.txt", 'r')
+        file = open("countries.txt",'r')
         await ctx.send(file.read())
         file.close()
 
-    @commands.command(name = 'help', aliases = ['h', '?', 'Help', 'HELP', 'guide', 'commands'])
-    async def helper(self, ctx, to_language='en'):
-        help_title = '**Command Guide**\n'
-        help_translate = '__ Translate __\n$tl|tr [from language] [to language] [message]\n'
-        help_translatetts = '__ Text-to-Speech __\n$tltts [from language] [to language] [message]\n'
-        help_languageguide = '__ Language Guide __\n$lg '
-        help_message = help_title + help_translate + help_translatetts + help_languageguide
-        translated_help_message = translate.google(help_message, 'en', str(to_language))
-        translated_help_message = translated_help_message.lower()
-        return await ctx.send(translated_help_message)
-
-
+    @commands.command(name = 'languageSearch', aliases = ['ls', 'Ls', 'lS', 'LS'])
+    async def languageSearch(self,ctx,*,language):
+        return await ctx.send(langDict[language.lower()])
 token  = configData["token"]
 def setup(bot):
     bot.add_cog(translator(bot))
 setup(bot)
 bot.run(token)
+
